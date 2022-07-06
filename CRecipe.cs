7256b31cfd8f6804ddac64b315c1140c0df2f080
lcudok <lenara-sophie.cudok@student.tu-freiberg.de>
2022-07-06 10:57:16 +0200

    #2 Create class Recipe

---
+++ CRecipe.cs
@@ -0,0 +1,10 @@
+public class Recipe
+{
+    public string? recipeName;
+    public string? recipeDescriptionShort;
+    public List<string>? recipeDescription;
+    public List<string>? ingredientList;
+
+    public void CreateNewRecipe(){}
+    public void addNewIngredient(){}
+}
