36f5dc0d0083973a6540734ff04572c174d50ee4
lcudok <lenara-sophie.cudok@student.tu-freiberg.de>
2022-07-12 16:43:54 +0200

    Create ingredient class #11

---
+++ Ingredient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FoodAppLibrary
+{
+    public class Ingredients: IEnumerable
+    {
+        public struct ingredient 
+        {
+            string name;
+            int id;
+        }
+        public List<ingredient> ingredients;
+
+
+        public IEnumerator<T> GetEnumerator<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
