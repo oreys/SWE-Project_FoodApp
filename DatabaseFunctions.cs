4a6016b78fed0d64a103b36cd713ad33a881a174
oreys <georg.fforberger@gmail.com>
2022-07-19 17:50:03 +0200

    #22 upload reorganized project

--- DatabaseFunctions.cs
+++ DatabaseFunctions.cs
@@ -9,15 +9,40 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Resources;
+using System.IO;
 
-namespace Food_App
+namespace FoodApp
 {
     public class DatabaseFunctions
     {
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=.\\food-app_database.mdf;Integrated Security=True";
+          
+        //connect to database
+        public void InitializeDatabaseFile()
+        {
+            string path = ".\\food-app_database.mdf";
+            bool databaseFound = File.Exists(path);
 
+            //check if database exists
+            //if existing, only return
+            if (databaseFound)
+            {
+                return;
+            }
+            //if not: take the database from resources
+            else
+            {
+                var database = FoodApp.Properties.Resources.food_app_database;
+                var fileStream = File.Create(path);
+                fileStream.Write(database, 0, database.Length);
+                fileStream.Close();
+            }
+        }
 
-        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=.\\food-app_database.mdf;Integrated Security=True";
-        //string connectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\georg\OneDrive\Desktop\Food-App\Food-App\food-app_database.mdf;Integrated Security=True";
-
+        public static string getConnectionString()
+        {
+            return connectionString;
+        }
     }
 }
---
+++ GUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodApp
+{
+    public partial class GUI : Form
+    {
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            if (btnHome.Visible == false)
+            {
+                btnHome.Visible = true;
+                btnSerchRecipe.Visible = true;
+                btnAddRecipe.Visible = true;
+            }
+            else
+            {
+                btnHome.Visible = false;
+                btnSerchRecipe.Visible = false;
+                btnAddRecipe.Visible = false;
+            }
+        }
+    }
+}
