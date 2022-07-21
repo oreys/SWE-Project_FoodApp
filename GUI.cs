2a17694682a51ba95366b230c1c8b6b762a1ca4e
oreys <georg.fforberger@gmail.com>
2022-07-21 15:57:34 +0200

    #15 add active function for userControls

--- GUI.cs
+++ GUI.cs
@@ -1,5 +1,15 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace FoodApp
 {
@@ -8,6 +18,12 @@ namespace FoodApp
         public GUI()
         {
             InitializeComponent();
+            Load += GUI_Load;
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            SetAvtivePanel(home1);
         }
 
         private void btnMenu_Click(object sender, EventArgs e)
@@ -28,24 +44,35 @@ namespace FoodApp
 
         private void btnAddRecipe_Click(object sender, EventArgs e)
         {
-            home1.Visible = false;
-            //searchRecipe.Visible = false;
-            addNewRecipe1.Visible = true;
+            SetAvtivePanel(addNewRecipe1);
         }
 
         private void btnHome_Click(object sender, EventArgs e)
         {
-            home1.Visible = true;
-            //searchRecipe.Visible = false;
-            addNewRecipe1.Visible = false;
+            SetAvtivePanel(home1);
         }
 
         private void btnSerchRecipe_Click(object sender, EventArgs e)
         {
+            //SetAvtivePanel(searchRecipe1);
+        }
+
+        /// <summary>
+        /// 
+        /// Sets the Active User Control and disables the other panels.
+        /// 
+        /// </summary>summary>
+        /// <param name="control"></param>
+        public void SetAvtivePanel(UserControl control)
+        {
+            // DISABLE ALL USEr CONTROLS //
+
             home1.Visible = false;
-            //searchRecipe1.Visible = true;
+            //searchRecipe1.Visible = false;
             addNewRecipe1.Visible = false;
 
+            // ENABLE THE ACTIVE CONTROL //
+            control.Visible = true;
         }
     }
 }
