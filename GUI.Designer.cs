61a437774f267bde49ce0c8078669c482eb98495
oreys <georg.fforberger@gmail.com>
2022-07-21 18:43:26 +0200

    fix merge problems

--- GUI.Designer.cs
+++ GUI.Designer.cs
@@ -41,9 +41,9 @@
             this.pnlMenu.SuspendLayout();
             this.pnlSite.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
             // pnlMenu
-            //
+            // 
             this.pnlMenu.Controls.Add(this.btnMenu);
             this.pnlMenu.Controls.Add(this.btnHome);
             this.pnlMenu.Controls.Add(this.btnSerchRecipe);
@@ -53,9 +53,9 @@
             this.pnlMenu.Name = "pnlMenu";
             this.pnlMenu.Size = new System.Drawing.Size(200, 744);
             this.pnlMenu.TabIndex = 0;
-            //
+            // 
             // btnMenu
-            //
+            // 
             this.btnMenu.Location = new System.Drawing.Point(12, 12);
             this.btnMenu.Name = "btnMenu";
             this.btnMenu.Size = new System.Drawing.Size(33, 33);
@@ -63,9 +63,9 @@
             this.btnMenu.Text = "≡";
             this.btnMenu.UseVisualStyleBackColor = true;
             this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
-            //
+            // 
             // btnHome
-            //
+            // 
             this.btnHome.Location = new System.Drawing.Point(25, 60);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(150, 33);
@@ -73,9 +73,9 @@
             this.btnHome.Text = "Home";
             this.btnHome.UseVisualStyleBackColor = true;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
-            //
+            // 
             // btnSerchRecipe
-            //
+            // 
             this.btnSerchRecipe.Location = new System.Drawing.Point(25, 120);
             this.btnSerchRecipe.Name = "btnSerchRecipe";
             this.btnSerchRecipe.Size = new System.Drawing.Size(150, 33);
@@ -83,9 +83,9 @@
             this.btnSerchRecipe.Text = "Search Recipe";
             this.btnSerchRecipe.UseVisualStyleBackColor = true;
             this.btnSerchRecipe.Click += new System.EventHandler(this.btnSerchRecipe_Click);
-            //
+            // 
             // btnAddRecipe
-            //
+            // 
             this.btnAddRecipe.Location = new System.Drawing.Point(25, 180);
             this.btnAddRecipe.Name = "btnAddRecipe";
             this.btnAddRecipe.Size = new System.Drawing.Size(150, 33);
@@ -93,9 +93,9 @@
             this.btnAddRecipe.Text = "Add Recipe";
             this.btnAddRecipe.UseVisualStyleBackColor = true;
             this.btnAddRecipe.Click += new System.EventHandler(this.btnAddRecipe_Click);
-            //
+            // 
             // pnlSite
-            //
+            // 
             this.pnlSite.Controls.Add(this.searchRecipe1);
             this.pnlSite.Controls.Add(this.home1);
             this.pnlSite.Controls.Add(this.addNewRecipe1);
@@ -104,35 +104,35 @@
             this.pnlSite.Name = "pnlSite";
             this.pnlSite.Size = new System.Drawing.Size(978, 744);
             this.pnlSite.TabIndex = 1;
-            //
+            // 
             // unitsTableAdapter1
-            //
+            // 
             this.unitsTableAdapter1.ClearBeforeFill = true;
-            //
+            // 
             // searchRecipe1
-            //
+            // 
             this.searchRecipe1.Location = new System.Drawing.Point(3, 0);
             this.searchRecipe1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.searchRecipe1.Name = "searchRecipe1";
             this.searchRecipe1.Size = new System.Drawing.Size(1105, 941);
             this.searchRecipe1.TabIndex = 2;
-            //
+            // 
             // home1
-            //
+            // 
             this.home1.Location = new System.Drawing.Point(0, 0);
             this.home1.Name = "home1";
             this.home1.Size = new System.Drawing.Size(978, 744);
             this.home1.TabIndex = 1;
-            //
+            // 
             // addNewRecipe1
-            //
+            // 
             this.addNewRecipe1.Location = new System.Drawing.Point(0, 0);
             this.addNewRecipe1.Name = "addNewRecipe1";
             this.addNewRecipe1.Size = new System.Drawing.Size(978, 744);
             this.addNewRecipe1.TabIndex = 0;
-            //
+            // 
             // GUI
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1178, 744);
