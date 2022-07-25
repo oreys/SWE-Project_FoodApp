0fe1fe1aba334b811681e08d1ac2beb9b9197543
lcudok <lenara-sophie.cudok@student.tu-freiberg.de>
2022-07-25 15:15:04 +0200

    Add documentation for Recipe, Ingredient, Step and RecipeService #35

--- Recipe.cs
+++ Recipe.cs
@@ -2,12 +2,28 @@ using System.Collections.Generic;
 
 namespace FoodApp
 {
+    /// <summary>
+    /// This class represents the basic structure of a recipe.
+    /// </summary>
     public class Recipe
     {
+        /// <summary>
+        /// Is a unique identifier for each recipe.
+        /// </summary>
         public int id;
+        /// <summary>
+        /// Represents the name of a recipe.
+        /// </summary>
         public string name;
+        /// <summary>
+        /// Represents a short description for a recipe.
+        /// </summary>
         public string description;
+        /// <summary>
+        /// Represents list of steps of a recipe.
+        /// </summary>
         public List<Step> steps;
+        /// Represents list of \ref Ingredient "ingredients" of a recipe.
         public List<Ingredient> ingredients;
 
 
--- Step.cs
+++ Step.cs
@@ -1,8 +1,17 @@
 ﻿namespace FoodApp
 {
+    /// <summary>
+    /// This class represents the basic structure of a step in a recipe.
+    /// </summary>
     public class Step
     {
+        /// <summary>
+        /// Represents the counter for all steps in one recipe.
+        /// </summary>
         public int number;
+        /// <summary>
+        /// Represents what should be done in that step.
+        /// </summary>
         public string description;
     }
 }
